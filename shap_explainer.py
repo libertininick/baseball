7969bb95ce9a1926af8db51d38bdf394dc1f93e8
libertininick <libertini.nick@gmail.com>
2021-01-01 06:01:55 -0700

    Add SHAP explainer module

---
+++ shap_explainer.py
@@ -0,0 +1,162 @@
+from collections import defaultdict
+import itertools
+import math
+
+import numpy as np
+import pandas as pd
+
+
+class SHAPExplainer():
+    """Tools for estimating the contribution of each input feature to a 
+    statistical model's predictions.
+
+    Can be used for local explanation of input features; i.e. explaining the impact of
+    of each feature for a single observation relative to some baseline or reference observation.
+
+    Or can be used to understand global importance of each feature across a set of observations.
+
+    Implementation is based on SHapley Additive exPlanations from "A Unified Approach to 
+    Interpreting Model Predictions" (https://arxiv.org/pdf/1705.07874.pdf).
+
+    Estimations of SHAP values implemented in a model agnostic way. 
+    """
+    
+    def __init__(self, prediction_fxn, n_feats, var_names=None, max_evals=2048):
+        """
+        Args:
+            prediction_fxn (function): Wrapper for model's prediction
+            n_feats (int): Number of input features to be explained
+            var_names (list): Input feature names
+            max_evals (int): Maximum number of model evaluations
+        """
+        self.prediction_fxn = prediction_fxn
+        self.n_feats = n_feats
+        self.var_names = var_names
+        self.max_evals = 2*self.n_feats + max_evals
+        
+        self.all_evals, self.feat_marginal_evals = self._enumerate_model_evaluations()
+
+        self.feat_toggle = np.zeros((len(self.all_evals), self.n_feats))
+        for i, toggle in enumerate(self.all_evals):
+            self.feat_toggle[i, toggle] = 1
+        
+    def _enumerate_model_evaluations(self):
+        """Enumerate a set of feature combinations from toggeling each input 
+        feature between a specific observed value and a reference/baseline value.
+        
+        These feature combinations are the combinations the model needs to be 
+        evaluated on to calculate the marginal impact each feature has on the 
+        output of the model.
+
+        Returns:
+            all_evals (set): Unique set of model evaluations
+            feat_marginal_evals (dict): List of marginal evaluations for each feature
+        """
+
+        # All possible feature combinations
+        n_evals = 2**self.n_feats 
+
+        all_evals = set()
+        feat_marginal_evals = defaultdict(list)
+        if n_evals <= self.max_evals:
+            # Enumerate every combination
+            feats = set(range(self.n_feats))
+            for f_i in feats:
+                # Get all other model features except feature i
+                other_feats = feats.difference([f_i])
+
+                # Enumerate each marginal calculation for feature i
+                for r in range(self.n_feats):
+                    # Combinations of model features before adding feature i
+                    for cmb in itertools.combinations(other_feats, r):
+                        # Model evaluations
+                        lhs = cmb                          # Before adding feature_i
+                        rhs = tuple(sorted(cmb + (f_i,)))  # After adding feature i
+                        all_evals.update([lhs, rhs])
+
+                        # Weighting for marginal sample based on # permutations
+                        prior_perms = max(1, math.factorial(r))
+                        after_perms = max(1, math.factorial(self.n_feats - r - 1))
+                        wt = prior_perms*after_perms
+
+                        # Tabulate marginal model evaluations
+                        feat_marginal_evals[f_i].append((lhs, rhs, wt))
+        else:
+            # Take a sample from possible permutations
+            while len(all_evals) < self.max_evals:
+                perm = np.random.permutation(self.n_feats)
+
+                for i in range(self.n_feats):
+                    f_i = perm[i]
+
+                    # Model evaluations
+                    lhs = tuple(sorted(perm[:i]))
+                    rhs = tuple(sorted(perm[:i+1]))
+                    all_evals.update([lhs, rhs])
+
+                    # Tabulate marginal model evaluations
+                    feat_marginal_evals[f_i].append((lhs, rhs, 1))
+
+        return all_evals, feat_marginal_evals
+    
+    def _evaluate_model(self, obs_values, reference_values):
+        # Feature combo matrix
+        combos = (
+            self.feat_toggle*(np.array(obs_values)[None, :]) + 
+            (1 - self.feat_toggle)*(np.array(reference_values)[None, :])
+        )
+        
+        # Model's prediction for each sample
+        yh = self.prediction_fxn(combos)
+        
+        # Evaluation map
+        model_evals = {
+            combo: yh[i]
+            for i, combo
+            in enumerate(self.all_evals)
+        }
+        
+        return model_evals
+    
+    def _est_feature_impacts(self, model_evals):
+        """Estimate the marginal impact of each feature to the model's prediction"""
+        feature_impacts = dict()
+        for f, f_evals in self.feat_marginal_evals.items():
+            impact, total_wt = 0, 0
+            for lhs, rhs, wt in f_evals:
+                impact += (model_evals.get(rhs) - model_evals.get(lhs))*wt
+                total_wt += wt
+            feature_impacts[f] = impact/total_wt
+            
+        return feature_impacts
+    
+    def shap_values(self, x, reference_values):
+        """Compute SHAP values for each observation (row) in input matrix x
+
+        Args:
+            x (ndarray): Matrix of input feature observations (n_obs, n_feats)
+            reference_values (ndarray): Reference or baseline value to compare each feature against (n_feats,)
+
+        Returns:
+            values (DataFrame): SHAP value for each feature for each observation in inputs x
+        """
+
+        # Compute SHAP values for each row
+        values = []
+        for row in x:
+            model_evals = self._evaluate_model(row, reference_values)
+            values.append(self._est_feature_impacts(model_evals))
+
+        # Result DataFrame
+        values = pd.DataFrame(values)
+        values = values[sorted(values.columns)]
+        if self.var_names:
+            inputs.columns = self.var_name
+            values.columns = self.var_names
+            
+        values['total'] = np.sum(values, axis=1)
+        yh = self.prediction_fxn(x)
+        values['yh'] = yh
+
+        return values
+        
